4cca4fe89ead07e127bff05d32bc7d54f6d8d3d5
Eisa Rezaei <e.rezaie@gmail.com>
2023-01-02 10:37:33 +0330

    add divider component

--- pages/dashboard/profile/index.tsx
+++ pages/dashboard/profile/index.tsx
@@ -1,12 +1,18 @@
 import Title from "components/common/Title";
 import ProfileBole from "public/images/icons/profile_bold.svg";
 import DashboardLayout from "components/layouts/dashboard";
+import Divider from "components/common/Divider";
 
 const Profile = () => {
   return (
     <DashboardLayout>
       <div className="profile-page-wrapper">
-      <Title titleIcon={ProfileBole} titleText="پروفایل" titleSideComponent={<div></div>}/>
+        <Title
+          titleIcon={ProfileBole}
+          titleText="پروفایل"
+          titleSideComponent={<div></div>}
+        />
+        <Divider />
       </div>
     </DashboardLayout>
   );

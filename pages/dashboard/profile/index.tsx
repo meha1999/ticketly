49794f0ec068e360bc64e50575d1dca86485d3f4
Eisa Rezaei <e.rezaie@gmail.com>
2023-01-02 15:10:54 +0330

    profile page

--- pages/dashboard/profile/index.tsx
+++ pages/dashboard/profile/index.tsx
@@ -2,6 +2,7 @@ import Title from "components/common/Title";
 import ProfileBole from "public/images/icons/profile_bold.svg";
 import DashboardLayout from "components/layouts/dashboard";
 import Divider from "components/common/Divider";
+import InputWithLabel from "components/pure/profile/InputWithLabel";
 
 const Profile = () => {
   return (
@@ -13,6 +14,37 @@ const Profile = () => {
           titleSideComponent={<div></div>}
         />
         <Divider />
+        <form className="user-profile-form">
+          <div className="form-item">
+            <InputWithLabel isFileInput id="user-detail" label="تصویر کاربری" />
+          </div>
+          <div className="form-item">
+            <InputWithLabel label="نام و نام خانوادگی" id="user-detail" />
+            <InputWithLabel label="شماره موبایل" id="user-cell" />
+          </div>
+          <div className="form-item">
+            <InputWithLabel label="کد ملی" id="national-code" />
+            <InputWithLabel label="ایمیل" id="email" />
+          </div>
+          <InputWithLabel
+            label="ادرس محل سکونت"
+            isFullWidthInput
+            id="address"
+          />
+          <div className="change-password">
+            <h4 className="change-pass-title" style={{ color: "#5E7BEC" }}>
+              تغییر رمز عبور
+            </h4>
+            <div className="form-item">
+              <InputWithLabel label="رمز عبور فعلی" id="current-pass" />
+              <InputWithLabel label="رمز عبور جدید" id="new-pass" />
+            </div>
+            <div className="form-item left">
+              <InputWithLabel label="تکرار رمز عبور جدید" id="new-pass" />
+            </div>
+          </div>
+          <Divider />
+        </form>
       </div>
     </DashboardLayout>
   );

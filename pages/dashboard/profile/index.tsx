0d516006b4e56d837c4ba63e1e80e0240e175de7
Meha 99 <m.epakchian@git.fn>
2023-01-02 14:44:35 +0330

    Add tickect list

--- pages/dashboard/profile/index.tsx
+++ pages/dashboard/profile/index.tsx
@@ -1,12 +1,12 @@
 import Title from "components/common/Title";
-import ProfileBole from "public/images/icons/profile_bold.svg";
+import ProfileBold from "public/images/icons/profile_bold.svg";
 import DashboardLayout from "components/layouts/dashboard";
 
 const Profile = () => {
   return (
     <DashboardLayout>
       <div className="profile-page-wrapper">
-      <Title titleIcon={ProfileBole} titleText="پروفایل" titleSideComponent={<div></div>}/>
+      <Title titleIcon={ProfileBold} titleText="پروفایل" titleSideComponent={<div></div>}/>
       </div>
     </DashboardLayout>
   );

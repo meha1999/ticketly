a742b8b1fee4f4d1f525dbca5d5ecf98b0d6f5fe
mahmoud <mahmoodh1371@gmail.com>
2023-01-04 16:42:41 +0330

    chat style done

--- src/components/common/chat/index.tsx
+++ src/components/common/chat/index.tsx
@@ -1,27 +1,64 @@
-import Link from "next/link";
-import Image from "next/image";
-import returnIcon from "images/icons/return_to_requests.svg";
 import VerticalPrevious from "images/icons/vertical_previous";
 import VerticalNext from "images/icons/vertical_next";
+import MessagePreview from "./message-preview";
+import img from "images/auth/admin.svg";
+import Message from "./message";
+
+const userType: Record<string, string> = {
+  evaluator: "#5E7BEC",
+  mechanic: "#00A48A",
+  supplier: "#F2C901",
+};
 
 const Chat = () => {
   return (
     <div className="chat">
       <div className="heading">
         <h2 className="title">{"لنت ترمز جلو پراید"}</h2>
-        <div>
-          <span>شناسه گفتگو:</span>
-          <span>TY4235689321</span>
+        <div className="chat-id">
+          <span>{"شناسه گفتگو:"}</span>
+          <span>{"TY4235689321"}</span>
         </div>
-        <div>
-          <button>
+        <div className="nav-buttons">
+          <button className="nav-button">
             <VerticalPrevious color="#00A48A" />
           </button>
-          <button>
+          <button className="nav-button">
             <VerticalNext color="#00A48A" />
           </button>
         </div>
       </div>
+      <div className="messages-list">
+        <MessagePreview
+          // profileImage={img}
+          color={userType["mechanic"]}
+          name="متین نوروزپور"
+          message="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد."
+          date="7 دی ماه 1401 13:19"
+        />
+        <MessagePreview
+          // profileImage={img}
+          color={userType["evaluator"]}
+          name="ارزیاب شماره 29"
+          message="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد."
+          date="7 دی ماه 1401 13:19"
+        />
+        <MessagePreview
+          // profileImage={img}
+          color={userType["evaluator"]}
+          name="ارزیاب شماره 29"
+          message="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد."
+          date="7 دی ماه 1401 13:19"
+        />
+      </div>
+      <MessagePreview
+        profileImage={img}
+        // color={userType["mechanic"]}
+        name="متین نوروزپور"
+        // message=""
+        date="7 دی ماه 1401 13:19"
+      />
+      <Message />
     </div>
   );
 };

ecf7d0839c9f5a063ac6c79104c9824380225239
Eisa Rezaei <e.rezaie@gmail.com>
2023-01-10 13:36:31 +0330

    fix pages role checking

--- pages/admin/dashboard/tickets/index.tsx
+++ pages/admin/dashboard/tickets/index.tsx
@@ -1,7 +1,20 @@
 import DashboardLayout from "components/layouts/dashboard/admin";
+import { GetServerSideProps } from "next";
 
 const Tickets = () => {
   return <DashboardLayout>Under Development</DashboardLayout>;
 };
 
 export default Tickets;
+
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  if (!ctx.req.url?.includes(ctx.req.cookies?.role as string)) {
+    ctx.res.setHeader("Location", "/admin/auth/login");
+    ctx.res.statusCode = 302;
+    ctx.res.end();
+  }
+  return {
+    props: {},
+  };
+};
--- pages/evaluator/dashboard/providers/index.tsx
+++ pages/evaluator/dashboard/providers/index.tsx
@@ -1,7 +1,19 @@
 import DashboardLayout from "components/layouts/dashboard/evaluator";
+import { GetServerSideProps } from "next";
 
 const Providers = () => {
   return <DashboardLayout>Under Development</DashboardLayout>;
 };
 
 export default Providers;
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  if (!ctx.req.url?.includes(ctx.req.cookies?.role as string)) {
+    ctx.res.setHeader("Location", "/evaluator/auth/login");
+    ctx.res.statusCode = 302;
+    ctx.res.end();
+  }
+  return {
+    props: {},
+  };
+};

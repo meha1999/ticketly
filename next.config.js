e64cff591d7eced783f25fa03305fbd87e3abf53
Meha 99 <m.epakchian@git.fn>
2023-01-17 12:01:44 +0330

    Fix bugs

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
+console.log(process.env.NEXT_PUBLIC_BASE_RASAD_WS_URL)
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [`${process.env.NEXT_PUBLIC_BASE_RASAD_WS_URL}`],
+    domains: [`${process.env.NEXT_PUBLIC_BASE_IMAGE_URL}`],
   },
 };
 

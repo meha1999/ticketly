2a81099f472a6cec31f2e7c8de98c97f3dcdac0e
Meha 99 <m.epakchian@git.fn>
2023-01-31 12:54:48 +0330

    Fix bugs

--- src/tools/error-handler.tsx
+++ src/tools/error-handler.tsx
@@ -2,16 +2,36 @@ import ToastComponent from "components/common/toast/ToastComponent";
 import { Toaster } from "components/common/toast/Toaster";
 
 const errorHandler = (error: any) => {
-  return error?.response?.data && Object.keys(error?.response?.data).length
-    ? Object.keys(error?.response?.data).map((item) => {
-        Toaster.error(
-          <ToastComponent
-            title={item}
-            description={error?.response?.data[item]}
-          />
+  try {
+    return error?.response?.data && Object.keys(error?.response?.data).length
+      ? Object.keys(error?.response?.data).map((item) => {
+          if (typeof error?.response?.data[item] === "string") {
+            Toaster.error(
+              <ToastComponent
+                title={item}
+                description={error?.response?.data[item]}
+              />
+            );
+          } else {
+            Toaster.error(
+              <ToastComponent
+                title="ناموفق"
+                description="خطای غیر منتظره رخ داده است"
+              />
+            );
+          }
+        })
+      : Toaster.error(
+          <ToastComponent title="ناموفق" description="خطای سرور" />
         );
-      })
-    : Toaster.error(<ToastComponent title="ناموفق" description="خطای سرور" />);
+  } catch (error) {
+    Toaster.error(
+      <ToastComponent
+        title="ناموفق"
+        description="خطای غیر منتظره رخ داده است"
+      />
+    );
+  }
 };
 
-export default errorHandler
+export default errorHandler;
